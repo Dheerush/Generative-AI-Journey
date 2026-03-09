bab7bbcb411b3877445274b02aef9eb81e142e33
Dheeraj Sharma <sharmadheeraj1996@gmail.com>
2026-03-10 00:32:10 +0530

    Initial commit - Start of Generative AI journey

---
+++ week1/1_intro.py
@@ -0,0 +1,49 @@
+# Writing first code 
+print("Namaste Dosto, Welcome to Python Programming")
+
+
+# Why Python ?
+# 1. Used for: web development (server-side), software development, mathematics, system scripting. 
+# 2. Our aim to learn python is to get into the field of Data Science and Machine Learning.
+# 3. Python is a high level programming language which is easy to learn and understand. Since we plan to learn LLMs, data science and machine learning, python is the best choice for us as it has a lot of libraries and frameworks for these fields.
+
+
+
+# Simple Rules :
+# 1. For comments we use #
+# 2. For printing we use print() function
+# 3. For writing code we use indentation (4 spaces)
+# 4. For writing multiple lines of code we use triple quotes (''' ''')
+# 5. For writing single line of code we use single quotes (' ') or double quotes (" ")
+
+
+
+# Python Syntax
+# Python Indentation : It means the spaces at the beginning of a code line. Python uses indentation to indicate a block of code. The standard is to use 4 spaces for indentation. It is important to be consistent with the number of spaces used for indentation in your code, as it can lead to syntax errors if not done correctly.
+# Example of indentation in Python:
+if 5 > 2 :
+    print("5 is greater than 2") # notice the spaces before print statement which indicates that it is part of the if block. If we remove the spaces, it will give an error.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
